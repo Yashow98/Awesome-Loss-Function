04175204b23e6d3150cdbf0bd1524faacaa3cdb2
Yashow98 <yashowhoo@outlook.com>
2023-05-17 22:21:50 +0800

    Add kld loss

--- torch_loss/kld_loss.py
+++ torch_loss/kld_loss.py
@@ -1,3 +1,18 @@
 # @Author  : YashowHoo
 # @File    : kld_loss.py
-# @Description : 
+# @Description :KL Divergence loss
+
+import numpy as np
+import torch
+from torch import nn
+
+
+def np_kl_loss(y_pred, y_true):
+    """
+    Calculate kl loss
+
+    :param y_pred:
+    :param y_true:
+    :return:
+    """
+    
